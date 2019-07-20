f8aaa90599df62a49ce48d58a6cc4a0232e516fd
kpphiri <kphiri6@gatech.edu>
2019-07-20 13:03:28 -0400

    Adding NavBar Component

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './NavBar.css';
+import { Route, Link } from 'react-router-dom';
+
+
+const navbar = ( props ) => {
+  return (
+    <div id="main">
+      <div className="topnav" style = {props.style}>
+        <a className="active navButton" href="/">Home</a>
+        <Link className="navButton" to="/Interests">Interests</Link>
+        < Link className="navButton" to="/Projects">Projects</Link>
+        <a className="navButton" href="/about">About</a>
+        <div id="right">Created with React.js ღ</div>
+      </div>
+    </div>
+  )
+}
+
+export default navbar;
