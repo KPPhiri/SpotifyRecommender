18673f86a17e6e41f9be17b7810afa0d13a0aab2
kpphiri <kphiri6@gatech.edu>
2019-07-26 18:57:50 -0400

    adding authentication

--- src/containers/PlayList/PlayList.js
+++ src/containers/PlayList/PlayList.js
@@ -4,27 +4,9 @@ import Track from '../../components/Track'
 
 class PlayList extends Component {
   state = {
-    tracks: [{name: "Cut To The Feeling", artist: "Carly Rae Jepsen", album: "Cut To The Feeling"},
-    {name: "1Cut To The Feeling", artist: "1Carly Rae Jepsen", album: "1Cut To The Feeling"},
-    {name: "2Cut To The Feeling", artist: "2Carly Rae Jepsen", album: "2Cut To The Feeling"},
-    {name: "3Cut To The Feeling", artist: "3Carly Rae Jepsen", album: "3Cut To The Feeling"},
-    {name: "4Cut To The Feeling", artist: "4Carly Rae Jepsen", album: "4Cut To The Feeling"},
-    {name: "5Cut To The Feeling", artist: "5Carly Rae Jepsen", album: "5Cut To The Feeling"},
-    {name: "6Cut To The Feeling", artist: "6Carly Rae Jepsen", album: "6Cut To The Feeling"},
-    {name: "7Cut To The Feeling", artist: "7Carly Rae Jepsen", album: "7Cut To The Feeling"},
-    {name: "8Cut To The Feeling", artist: "8Carly Rae Jepsen", album: "8Cut To The Feeling"},
-    {name: "9Cut To The Feeling", artist: "9Carly Rae Jepsen", album: "9Cut To The Feeling"},
-    {name: "10Cut To The Feeling", artist: "10Carly Rae Jepsen", album: "10Cut To The Feeling"},
-    {name: "1Cut To The Feeling", artist: "1Carly Rae Jepsen", album: "1Cut To The Feeling"},
-    {name: "2Cut To The Feeling", artist: "2Carly Rae Jepsen", album: "2Cut To The Feeling"},
-    {name: "3Cut To The Feeling", artist: "3Carly Rae Jepsen", album: "3Cut To The Feeling"},
-    {name: "4Cut To The Feeling", artist: "4Carly Rae Jepsen", album: "4Cut To The Feeling"},
-    {name: "5Cut To The Feeling", artist: "5Carly Rae Jepsen", album: "5Cut To The Feeling"},
-    {name: "6Cut To The Feeling", artist: "6Carly Rae Jepsen", album: "6Cut To The Feeling"},
-    {name: "7Cut To The Feeling", artist: "7Carly Rae Jepsen", album: "7Cut To The Feeling"},
-    {name: "8Cut To The Feeling", artist: "8Carly Rae Jepsen", album: "8Cut To The Feeling"},
-    {name: "9Cut To The Feeling", artist: "9Carly Rae Jepsen", album: "9Cut To The Feeling"}]
+    tracks: this.props.tracks
   }
+
 
 
 
@@ -33,7 +15,7 @@ class PlayList extends Component {
       <div id="PlayListContainer">
       {this.state.tracks.map((track, index) => {
         return (
-          <Track name={track.name} artist={track.artist} album={track.album}></Track>
+          <Track movetrack1totrack2={this.props.movetrack1totrack2.bind(this)}  playlist_type = {this.props.playlist_type} name={track.name} artist={track.artist} album={track.album}></Track>
         )
       })}
       </div>
