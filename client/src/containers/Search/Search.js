a8b0e873269f757e87700ba7823bed01ca9b8bc3
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-07-27 19:24:09 -0400

    add Search component to containers

---
+++ client/src/containers/Search/Search.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Search extends Component {
+
+    //Store state of current information
+    state = {
+        searchField :'',
+        listOfSongs : [],
+        features: []
+    }
+
+    //Store user search input in state searchfield 
+    
+
+    //Make API call to spotify to get the list of songs that match the search entered by user
+    //  store the returned list in the listOfSongs array
+
+
+    //Grab the song Id the user selected and place it in state
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
